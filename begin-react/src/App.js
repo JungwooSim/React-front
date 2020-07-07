04260b4e3f3d997c346e133311f15617e487022b
JungwooSim <cscd053@gmail.com>
2020-07-07 21:19:16 +0900

    13. 배열에 항목 추가하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,8 +1,23 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username : '',
+    email : ''
+  });
+
+  const {username, email} = inputs;
+
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name] : value
+    });
+  }
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -18,16 +33,36 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
-
+  ]);
   const nextId = useRef(4);
 
   const onCreate = () => {
+    const user = {
+      id : nextId.current,
+      username,
+      email
+    };
+    // setUsers([...users, user]); // spread 연산자를 이용한 추가 방법
+    setUsers(users.concat(user)); // concat 햠수를 이용한 연산법 방
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+
     nextId.current += 1;
   };
 
   return (
-      <UserList users={users}/>
+      <>
+        <CreateUser
+          username={username}
+          email={email}
+          onChange={onChange}
+          onCreate={onCreate}
+        />
+        <UserList users={users}/>
+      </>
   );
 }
 
