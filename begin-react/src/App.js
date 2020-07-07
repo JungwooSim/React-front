85590e70086cbd3c2a025604bb74da2dbb0bd45f
JungwooSim <cscd053@gmail.com>
2020-07-07 21:34:04 +0900

    14. 배열에 항목 제거하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -53,6 +53,10 @@ function App() {
     nextId.current += 1;
   };
 
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  }
+
   return (
       <>
         <CreateUser
@@ -61,7 +65,7 @@ function App() {
           onChange={onChange}
           onCreate={onCreate}
         />
-        <UserList users={users}/>
+        <UserList users={users} onRemove={onRemove}/>
       </>
   );
 }
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-function User({user}) {
+function User({user, onRemove}) {
     return (
         <div>
             <b>{user.username}</b> <span>({user.email})</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     )
 }
 
-function UserList({users}) {
+function UserList({users, onRemove}) {
     return (
         <div>
             {users.map((user,index) => (
-                <User user={user} key={index}/>
+                <User user={user} key={index} onRemove={onRemove}/>
             ))}
         </div>
     )
