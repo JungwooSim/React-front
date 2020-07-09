69227d9cb6eccbce5b3d93e1d31a567e19177bff
JungwooSim <cscd053@gmail.com>
2020-07-09 12:48:08 +0900

    23. Immer 를 사용한 더 쉬운 불변성 관리

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,6 +2,7 @@ import React, {useCallback, useMemo, useReducer, useRef} from 'react';
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 import useInputs from "./hooks/useInputs";
+import produce from "immer";
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -37,31 +38,34 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'CHANGE_INPUT' :
-      return {
-        ...state,
-        inputs : {
-          ...state.inputs,
-          [action.name] : action.value
-        }
-      };
     case 'CREATE_USER' :
-      return {
-        inputs : initialState.inputs,
-        users : state.users.concat(action.user)
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
+      // return {
+      //   inputs : initialState.inputs,
+      //   users : state.users.concat(action.user)
+      // };
     case 'TOGGLE_USER' :
-      return {
-        ...state,
-        users : state.users.map(user =>
-          user.id === action.id ? {...user, active : !user.active} : user
-        )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
+      // return {
+      //   ...state,
+      //   users : state.users.map(user =>
+      //     user.id === action.id ? {...user, active : !user.active} : user
+      //   )
+      // };
     case 'REMOVE_USER' :
-      return {
-        ...state,
-        users : state.users.filter(user => user.id !== action.id)
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+      // return {
+      //   ...state,
+      //   users : state.users.filter(user => user.id !== action.id)
+      // };
     default :
       return state;
   }
