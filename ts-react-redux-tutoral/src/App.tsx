b929d687aa05b19b78ef980957ee654d88645b71
JungwooSim <cscd053@gmail.com>
2020-08-24 01:04:26 +0900

    Todo 개발

--- ts-react-redux-tutoral/src/App.tsx
+++ ts-react-redux-tutoral/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CounterContainer from "./containers/CounterContainer";
+import TodoApp from "./containers/TodoApp";
 
 function App() {
   return (
-      <CounterContainer />
+      // <CounterContainer />
+      <TodoApp />
   );
 }
 
